8f504f10ba8bb32ab17d20dfa6260fc63f5af261
Joarder Kamal <joarder@ymail.com>
2014-03-06 20:56:39 +1100

    Commit

--- DDBMSsim/src/jkamal/ddbmssim/main/DBMSSimulator.java
+++ DDBMSsim/src/jkamal/ddbmssim/main/DBMSSimulator.java
@@ -38,7 +38,7 @@ public class DBMSSimulator {
 	public final static int PARTITION_MAX_CAPACITY = 1000; // in data rows
 	
 	public final static int TRANSACTIONS = 1000;
-	public final static int SIMULATION_RUNS = 2;
+	public final static int SIMULATION_RUNS = 24;
 
 	public final static int TPCC_WAREHOUSE = 10; // # of Warehouse, W = 1+	
 	public final static double TPCC_Scale = 0.001; // Reflects the total number of Data Rows in each Table; 0.001 = 1/1K
