be73e17a0236238d9258264d19af572945f2442c
Joarder Kamal <joarder@ymail.com>
2014-05-15 05:55:06 +1000

    NC Run With All Partitions

--- DDBMSsim/src/jkamal/ddbmssim/main/DBMSSimulator.java
+++ DDBMSsim/src/jkamal/ddbmssim/main/DBMSSimulator.java
@@ -293,10 +293,10 @@ public class DBMSSimulator {
 		//sampled_workload.show(db, "");
 		
 		//Perform Data Stream Mining to find the transactions containing Distributed Semi-Frequent Closed Itemsets (tuples)		
-		write("Starting data stream mining to identify the transactions containing distributed semi-frequent closed sets of data tuples ...", "ACT");
+		//write("Starting data stream mining to identify the transactions containing distributed semi-frequent closed sets of data tuples ...", "ACT");
 		simulation_logger.logTimings(db.getTiming_log(), "start");
-		int target_transactions = streamMiner.mining(db, sampled_workload, simulation_logger, DBMSSimulator.LOG_LOCATION);
-		simulation_logger.logTimings(db.getTiming_log(), "stop");
+		//int target_transactions = streamMiner.mining(db, sampled_workload, simulation_logger, DBMSSimulator.LOG_LOCATION);
+		simulation_logger.logTimings(db.getTiming_log(), "skip");
 		
 		// Perform transaction classification
 		// Classify the workload transactions based on whether they are distributed or not (Red/Orange/Green List)
